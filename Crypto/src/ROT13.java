bf2d4b7532abdea8c9d9339f073c6dce5b7e0362
Henry <cataniahf@gmail.com>
2019-06-24 16:37:32 -0400

    All tests passing, needs ceasar and personal cipher.

--- Crypto/src/ROT13.java
+++ Crypto/src/ROT13.java
@@ -3,8 +3,10 @@ import static java.lang.Character.isUpperCase;
 import static java.lang.Character.toLowerCase;
 
 public class ROT13  {
+    Integer shift;
 
     ROT13(Character cs, Character cf) {
+       shift = cs - cf;
     }
 
     ROT13() {
@@ -12,21 +14,42 @@ public class ROT13  {
 
 
     public String crypt(String text) throws UnsupportedOperationException {
-
-        return "";
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < text.length(); i++) {
+            char c = text.charAt(i);
+            if (c >= 'a' && c <= 'm') c += 13;
+            else if (c >= 'A' && c <= 'M') c += 13;
+            else if ( c >= 'n' && c <= 'z') c -= 13;
+            else if (c >= 'N' && c <= 'Z') c -= 13;
+            sb.append(c);
+        }
+        return sb.toString();
     }
 
     public String encrypt(String text) {
-        return text;
+        return crypt(text);
     }
 
     public String decrypt(String text) {
-        return text;
+        return crypt(text);
     }
 
     public static String rotate(String s, Character c) {
-
-        return "";
+        int index = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == c){
+                index = i;
+                break;
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = index; i < s.length(); i++) {
+            sb.append(s.charAt(i));
+        }
+        for (int i = 0; i < index; i++) {
+            sb.append(s.charAt(i));
+        }
+        return sb.toString();
     }
 
 }
